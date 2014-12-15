225736ace5f6e3dfda1d1e6aabf418e9f0b82e43
Hardik Shah <mail.hardikshah@gmail.com>
2014-12-16 02:17:20 +0530

    users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,16 +15,30 @@ class CreateUsersTable extends Migration {
 		Schema::create('users', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('username');
+			$table->bigInteger('fbid')->nullable();
+			$table->bigInteger('gid')->nullable();
+			$table->bigInteger('twid')->nullable();
+			$table->bigInteger('gitid')->nullable();
+			$table->string('username')->index()->unique();
 			$table->string('name')->nullable();
 			$table->string('first_name')->nullable();
 			$table->string('last_name')->nullable();
-			$table->string('email')->unique();
+			$table->string('email')->index()->unique();
 			$table->string('password', 60);
 			$table->boolean('status')->default(0);
+			$table->boolean('chat_status')->default(0);
 			$table->enum('gender',array('M','F','N'));
 			$table->date('dob')->nullable();
-			$table->text('bio')->nullable();
+			$table->string('avatar')->nullable();
+			$table->integer('country')->default(1);
+			$table->string('url')->nullable();
+			$table->string('fb_link')->nullable();
+			$table->string('tw_link')->nullable();
+			$table->string('insta_link')->nullable();
+			$table->string('skype')->nullable();
+			$table->string('phone')->nullable();
+			$table->string('mobile')->nullable();
+			$table->text('bio')->nullable()->nullable();
 			$table->date('last_login');
 			$table->boolean('verified')->default(0);
 			$table->boolean('tour')->default(0);
